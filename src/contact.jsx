e33523706ae1ebfe08fed157e317cde2a0daff70
Karmanya Singh <karmanyasingh8@gmail.com>
2024-01-18 11:14:40 +0530

    better ui.Scroll contact not working

--- src/contact.jsx
+++ src/contact.jsx
@@ -5,8 +5,21 @@ const Contact = () => {
     <footer className="contact-footer">
       <div className="footer-content">
         <h2>Contact Us</h2>
-        <p>If you have any questions or feedback, feel free to reach out!</p>
+        <p>If you have any questions or feedback, feel free to reach out!</p> 
       </div>
+      <div className="contact-name1">
+        <h3>Vikas Chouhan</h3>
+        <h4>+91 7357303139</h4>
+      </div>
+      <div className="contact-name2">
+        <h3>Khushi Pandey</h3>
+        <h4>+91 56556548462</h4>
+      </div>
+      <div className="contact-name3">
+        <h3>Karmanya Singh</h3>
+        <h4>+91 9999098900</h4>
+      </div>
+      
     </footer>
   );
 };
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,74 +1,22 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faInfo, faUsers, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-library.add(faHome, faInfo, faUsers, faEnvelope);
+import { faHome, faInfo, faHandshake, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const CustomNavbar = () => {
-  const scrollToSponsors = () => {
-    const sponsorsSection = document.getElementById('sponsors');
-    if (sponsorsSection) {
-      sponsorsSection.scrollIntoView({ behavior: 'smooth' });
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
     }
-  };
-
-  const scrollToHome = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-
-  const scrollToAbout = () => {
-    const aboutSection = document.getElementById('about');
-    if (aboutSection) {
-      aboutSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
-  const scrollToContact = () => {
-    const documentHeight = document.documentElement.scrollHeight;
-  
-    window.scrollTo({
-      top: documentHeight,
-      behavior: 'smooth',
-    });
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="sticky-top">
-      <Navbar.Brand href="/" className="navbar-brand">
-        <img
-          src="glitch.jpeg"
-          alt="Logo"
-          height="30"
-          className="d-inline-block align-top"
-        />
-      </Navbar.Brand>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="flex-column ml-auto">
-          <Nav.Link onClick={scrollToHome} className="nav-link">
-            <FontAwesomeIcon icon={faHome} style={{ fontSize: '30px', marginRight: '8px' }} />
-            Home
-          </Nav.Link>
-          < br />
-          <Nav.Link onClick={scrollToAbout} className="nav-link">
-            <FontAwesomeIcon icon={faInfo} style={{ fontSize: '30px', marginRight: '8px' }} />
-            About Us
-          </Nav.Link>
-          < br />
-          <Nav.Link onClick={scrollToSponsors} className="nav-link">
-            <FontAwesomeIcon icon={faUsers} style={{ fontSize: '30px', marginRight: '8px' }} />
-            Our Sponsors
-          </Nav.Link>
-          < br />
-          <Nav.Link onClick={scrollToContact} className="nav-link">
-            <FontAwesomeIcon icon={faEnvelope} style={{ fontSize: '30px', marginRight: '8px' }} />
-            Contact Us
-          </Nav.Link>
-          < br />
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className="floating-icons">
+      <div onClick={() => scrollToSection('home')}><FontAwesomeIcon icon={faHome} /></div>
+      <div onClick={() => scrollToSection('about')}><FontAwesomeIcon icon={faInfo} /></div>
+      <div onClick={() => scrollToSection('sponsors')}><FontAwesomeIcon icon={faHandshake} /></div>
+      <div onClick={() => scrollToSection('contact')}><FontAwesomeIcon icon={faEnvelope} /></div>
+    </div>
   );
 };
 
