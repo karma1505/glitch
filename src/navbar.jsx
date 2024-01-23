484c1e4790ad49080daf889315cc90a86cbc5e24
Karmanya Singh <karmanyasingh8@gmail.com>
2024-01-23 12:50:11 +0530

    socials

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUsers, faHandshake, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'; // Use free-brands-svg-icons for social media icons
 
 const CustomNavbar = () => {
   const scrollToHome = () => {
@@ -15,7 +16,7 @@ const CustomNavbar = () => {
   };
 
   const scrollToSponsors = () => {
-    const sponsorSection= document.getElementById('sponsors');
+    const sponsorSection = document.getElementById('sponsors');
     if (sponsorSection) {
       sponsorSection.scrollIntoView({ behavior: 'smooth' });
     }
@@ -38,6 +39,14 @@ const CustomNavbar = () => {
       <div onClick={() => scrollToAbout()}><FontAwesomeIcon icon={faUsers} /></div>
       <div onClick={() => scrollToSponsors()}><FontAwesomeIcon icon={faHandshake} /></div>
       <div onClick={() => scrollToContact()}><FontAwesomeIcon icon={faEnvelope} /></div>
+    <div className="social-icons">
+      <a href="https://www.instagram.com/glitch.shoolini/" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
+      <a href="https://twitter.com/glitchshoolini" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+    </div>
     </div>
   );
 };
