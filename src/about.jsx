8aa0eb22d9e0daaade6e77edb9024f2836f588d4
Karmanya Singh <karmanyasingh8@gmail.com>
2024-01-25 10:56:16 +0530

    css changes

--- src/about.jsx
+++ src/about.jsx
@@ -10,8 +10,7 @@ const About = () => {
         Join us at Glitch 24, the ultimate tech fest showcasing exhilarating competitions like Robowars, Micro Mouse Maze, and CTF challenges. Immerse yourself in cutting-edge technology, innovation, and friendly competition. Explore the future of robotics, conquer coding challenges, and witness mind-bending tech battles. 
         </p>
         <p>
-        Unleash your skills, connect with like-minded enthusiasts, and experience the thrill of Glitch 24, where technology and talent collide for an unforgettable celebration of innovation. Don't miss the chance to be part of this transformative experience, pushing the boundaries of what's possible in the world of technology.
-        unforgettable celebration of innovation.
+        Unleash your skills, connect with like-minded enthusiasts, and experience the thrill of Glitch 24, where technology and talent collide for an unforgettable celebration of innovation.
         </p>
       </div>
       <div className="about-tracker">
